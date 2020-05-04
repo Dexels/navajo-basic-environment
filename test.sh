917c6d763fa5d0362e65f92c3a2a35a951077fee
Gerco Ballintijn <gballintijn@dexels.com>
2020-05-04 16:20:06 +0200

    Fixed test calling script (with new repository name).

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@
 #
 
 VOLUME="$PWD/surefire-reports:/surefire-reports"
-NETWORK="navajoexample_default"
+NETWORK="navajo-basic-environment_default"
 IMAGE="dexels/navajo.example.test:latest"
 
 docker  run  -v $VOLUME  --network $NETWORK  $IMAGE
